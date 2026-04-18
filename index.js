1f304555f0fb3ccbf34175f398ae9efe87456e98
Satyanarayan Sen <satyanarayan74sen@gmail.com>
2026-04-18 21:05:28 +0530

    UI update

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-const btn = document.getElementById("btn");
-const themeBtn = document.getElementById("themeBtn");
-
-btn.addEventListener("click", () => {
-  btn.innerText = "🔥 Let's Go!";
-});
-
-themeBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-
-  themeBtn.innerText = document.body.classList.contains("dark")
-    ? "☀️"
-    : "🌙";
-});
+console.log("Amplify site loaded 🚀");
